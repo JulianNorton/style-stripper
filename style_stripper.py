11035e2c41677bdc3bce95349cec47ba9ed38c6b
Julian Norton <Julian@JulianNorton.com>
2016-05-25 17:31:53 -0400

    calculating final compression, comments, and removing prints

--- style_stripper.py
+++ style_stripper.py
@@ -1,6 +1,7 @@
-def read_txt():
+def style_stripper():
 
-    print('\n\n\n\n')
+    print('\n\n')
+
     ###########################
     # importing unused styles
     list_styles = list()
@@ -15,62 +16,77 @@ def read_txt():
     before_stylesheet = before_stylesheet_file.readlines()
     before_stylesheet_file.close()
 
-    # index = 0
+    ###########################
+    # Cleaning audit list for bad commas
     for css_selector in list_styles:
-        # print(list_styles[index])
-        # print(index)
-        # index = index + 1
         if css_selector[-1] == ',':
-            # print('BEFORE', css_selector)
-            # print(css_selector[-1], '== Comma!')
             css_selector = css_selector[:-1]
-            # print('AFTER', css_selector, '\n')
-        # else:
-        #     print(css_selector)
-        #     print(css_selector[-1], '|| No comma \n')
+
+    after_stylesheet = list()
+    def selector_processor(x):
+        i = 0
+        did_find = list()
+        while i < len(before_stylesheet):
+            selector = str(x)
+            # Chunks are each 'item' in before_stylesheet
+            # If there are no whitespaces, there will only be 1 chunk.
+            stylesheet_chunk = before_stylesheet[i]
+
+            # Try and find location of the selector
+            try:
+                selector_location_beginning = stylesheet_chunk.index(selector)
+                # print(selector_location_beginning, ' || selector location begins')
+
+                # Find range of styles for that selector
+                styles_index_beginning = stylesheet_chunk[selector_location_beginning:].index('{')
+                # +1 for index because we want to include the '}'
+                styles_index_end = stylesheet_chunk[selector_location_beginning:].index('}') + 1 
+
+                # print(styles_index_end, ' || how long the selection + styles are')
+                # Ran
+                selector_range = stylesheet_chunk[selector_location_beginning: selector_location_beginning + styles_index_end]
+                selector_range_index = selector_location_beginning + styles_index_end
+                # print(selector_range, '|| selection range')
+
+
+                # print('length before:        ', len(stylesheet_chunk))
+                stylesheet_chunk = stylesheet_chunk[:selector_location_beginning] + stylesheet_chunk[selector_range_index:]
+                # print('length after deletion:' ,len(stylesheet_chunk))
+
+                # If it's found, save which chunk it was found in
+                did_find.append(i)
+
+            except:
+                # print(selector, 'not found in stylesheet_chunk', '#',i)
+                pass
+
+            after_stylesheet.append(stylesheet_chunk)
+            i += 1
+
+        if did_find != list():
+            # print('Chunks edited:', did_find)
+            pass
+        else:
+            # print('No chunks edited')
+            pass
+
 
     ###########################
-    # Find range of selector and values
-    
-    # Temporary values
-    selector = 'body'
-    test_list = before_stylesheet[0]
+    # Searches all of the stylesheet chunks for the selectors
+    for selector in range(0,len(list_styles)):
+        # print(selector)
+        selector_processor(selector)
 
-    # Find location of selector
-    location_index = test_list.index(selector)
-    selector_location_beginning = location_index
-    print(selector_location_beginning, ' || selector location begins')
-    # selector_location_end = location_index + len(selector)
-    # print(selector_location_end)
+    ###########################
+    # Is the newer stylesheet smaller than the new one?
+    # print('before_stylesheet', len(before_stylesheet.join()))
+    before_stylesheet = ''.join(before_stylesheet)
+    print('Char length Before', len(before_stylesheet))
+    print('Char length After', len(after_stylesheet))
 
-    # Find range of styles for that selector
-    styles_index_beginning = test_list[selector_location_beginning:].index('{')
-    styles_index_end = test_list[selector_location_beginning:].index('}') + 1 # +1 because we want to include the '}'
+    print('Compressed %: ', len(after_stylesheet)/len(before_stylesheet))
 
-    print(styles_index_end, ' || how long the selection + styles are')
-    selector_range = test_list[selector_location_beginning: selector_location_beginning + styles_index_end]
-    selector_range_index = selector_location_beginning + styles_index_end
-    print(selector_range, 'selection range')
-
-    print(test_list, '\n\n')
-
-
-    print(test_list[:selector_location_beginning])
-    print(test_list[selector_range_index:])
     print('\n')
-    test_list = test_list[:selector_location_beginning] + test_list[selector_range_index:]
-    print(test_list)
 
-    print('\n\n\n\n')
-
-    
-
-
-
-
-
-
-
-
-read_txt()
+style_stripper()
 
