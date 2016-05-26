b39bb99c84591a7272738eb5504830f0732e713c
Julian Norton <Julian@JulianNorton.com>
2016-05-26 11:45:46 -0400

    Cleaning up prints

--- style_stripper.py
+++ style_stripper.py
@@ -36,41 +36,21 @@ def style_stripper():
             css_selector = css_selector[:-1]
 
         
-    # print(len(list_styles))
-    # for selector_index in range(0,len(list_styles)):
-    # print(len(before_stylesheet), len(after_stylesheet), 'len before/after')
     for selector_index in range(0,len(list_styles)):
-    # for selector_index in range(0,4):
-        # print(list_styles[selector_index])
         selector = list_styles[selector_index]
         # Try and find location of the selector
         if selector in after_stylesheet:
-            print('\n', selector)
-            # print(selector, 'selector found!')
             selector_location_beginning = after_stylesheet.index(selector)
-            print(after_stylesheet.index(selector), 'selector beginning index')
-            # print(before_stylesheet.index(selector))
-            # # Single bracket?
-            # print('single bracket found')
-            # print(after_stylesheet)
-            # closing_bracket_index = selector_location_beginning
             closing_bracket_index = after_stylesheet.index('}', selector_location_beginning) + 1
+            # Look for a double closing bracket
             if after_stylesheet[closing_bracket_index-1:closing_bracket_index] == '}':
-                print('one bracket found')
                 try:
                     if after_stylesheet[closing_bracket_index-1:closing_bracket_index + 1] == '}}':
-                        print(after_stylesheet[closing_bracket_index-1:closing_bracket_index + 1])
-                        print('double bracket', selector)
-                        print(closing_bracket_index)
                         closing_bracket_index += 1
-                        print(closing_bracket_index)
                 except:
                     print('EXCEPTION')
-            print(closing_bracket_index, 'closing bracket index')
             range_to_delete = after_stylesheet[selector_location_beginning:closing_bracket_index]
-            print('del -->|',range_to_delete,'|<-- del')
             after_stylesheet = after_stylesheet[:selector_location_beginning] + after_stylesheet[closing_bracket_index:]
-            # print('\n', after_stylesheet, '\n')
         else:
             print('selector not found', selector)
 
@@ -92,12 +72,11 @@ def style_stripper():
     ##########################
     # Size comparison
     statinfo_original = os.stat('original_stylesheet.css').st_size
-    statinfo_before = os.stat('before_stylesheet.css').st_size
+    # statinfo_before = os.stat('before_stylesheet.css').st_size
     statinfo_after = os.stat('clean.css').st_size
-    print('\n')
-    print(statinfo_original/1000, '~ kilobytes || original css')
-    print(statinfo_before/1000, '~ kilobytes || before css')
-    print(statinfo_after/1000, '~ kilobytes || clean css')
+    print('original css (KB) ==', statinfo_original/1000)
+    # print(statinfo_before/1000, '~ kilobytes || before css')
+    print('Clean css (KB)   == ', statinfo_after/1000)
 
 
 style_stripper()
